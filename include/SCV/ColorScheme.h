0cda54cd5dd386787e42d2818e5fa6f5ac887310
yuriks <yuriks@yuriks.net>
2012-03-12 02:55:20 +0000

    Added documentation and simplified code for ColorScheme

--- include/SCV/ColorScheme.h
+++ include/SCV/ColorScheme.h
@@ -1,3 +1,9 @@
+/*!
+\file       ColorScheme.h
+\brief
+\author     SCV Team
+*/
+
 #ifndef __SCV_COLOR_SCHEME_H__
 #define __SCV_COLOR_SCHEME_H__
 
@@ -6,28 +12,34 @@
 
 namespace scv {
 
+/*! Functionality to customize SCV component color themes.
+
+Since it's a Singleton class, must be acessed via getInstance().
+\example scv::ColorScheme::getInstance()->loadScheme(scv::ColorScheme::scvDefault);
+*/
 class ColorScheme : public Singleton<ColorScheme> {
 friend class Singleton<ColorScheme>;
 public:
-   ///////////////////////////////////////////////////////////
+   //! Possible color values that can be customized.
    enum Target {
-      CURSOR,
-      TEXT,
-      TEXTSELECTION,
-      TEXTSELECTED,
-      BACKGROUND,
-      MENUBAR,
-      PROGRESSBAR,
-      SEPARATOR,
-      TEXTFIELD,
-      CONTEXTMENU,
-      CONTEXTMENUFONT,
-      PANEL,
-      SCROLLPANEL,
-      MAINCOMPONENTS,
-      OVERCOMPONENTS,
-      HOLDCOMPONENTS,
-      FONT
+      CURSOR,          //!< SCV cursor
+      TEXT,            //!< Text color
+      TEXTSELECTION,   //!< Color of text selection rectangle
+      TEXTSELECTED,    //!< Color of selected text
+      BACKGROUND,      //!< Main window background
+      MENUBAR,         //!< widget
+      PROGRESSBAR,     //!< ProgressBar widget
+      SEPARATOR,       //!< Separator widget
+      TEXTFIELD,       //!< TextField widget
+      CONTEXTMENU,     //!< Context menus
+      CONTEXTMENUFONT, //!< Context menu text
+      PANEL,           //!< Panel background
+      SCROLLPANEL,     //!< Scrollable panel background
+      MAINCOMPONENTS,  // if not specified like a special widget
+      OVERCOMPONENTS,  // if not specified like a special widget
+      HOLDCOMPONENTS,  // if not specified like a special widget
+      FONT,            //!< General text
+      s_nComponents    //!< Total number of color settings
    };
 
    //! Predefined color schemes
@@ -40,31 +52,21 @@ public:
    };
    ///////////////////////////////////////////////////////////
 
-   ///////////////////////////////////////////////////////////
    void loadScheme(ColorScheme::SchemeStyle style);
-   ///////////////////////////////////////////////////////////
 
-   ///////////////////////////////////////////////////////////
    void setColor(ColorScheme::Target target, const Color4f &color);
    const Color4f& getColor(ColorScheme::Target target) const;
-   ///////////////////////////////////////////////////////////
 
-   ///////////////////////////////////////////////////////////
    void applyColor(const Color4f &color);
    void applyColor(ColorScheme::Target target);
    void applyColor(ColorScheme::Target target, float w);
-   ///////////////////////////////////////////////////////////
 
-   void applyDefaultModulate(void);
+   void applyDefaultModulate();
 
 private:
-   ///////////////////////////////////////////////////////////
-   ColorScheme(void);
-   ~ColorScheme(void);
-   ///////////////////////////////////////////////////////////
+   ColorScheme();
 
-   static const int s_nComponents = 17;
-   Color4f *_pColors[s_nComponents];
+   Color4f _pColors[s_nComponents];
    const Color4f _defaultModulateColor;
 };
 
--- src/ColorScheme.cpp
+++ src/ColorScheme.cpp
@@ -5,95 +5,90 @@
 
 namespace scv {
 
-ColorScheme::ColorScheme(void) : _defaultModulateColor(1.f, 1.f, 1.f, 1.f) {
-   std::memset(_pColors, NULL, sizeof(Color4f*) * s_nComponents);
+ColorScheme::ColorScheme() : _defaultModulateColor(1.f, 1.f, 1.f, 1.f) {
    loadScheme(SCVDEFAULT);
 }
 
-ColorScheme::~ColorScheme(void) {
-   for (int i = 0; i < s_nComponents; i++)
-      delete _pColors[i];
-}
-
+//! Load a predefined SCV color scheme
 void ColorScheme::loadScheme(ColorScheme::SchemeStyle style) {
    switch (style) {
    case SCVDEFAULT:
-      _pColors[CURSOR]          = new Color4f(ColorRGBA(255, 255,   0, 255));
-      _pColors[TEXT]            = new Color4f(ColorRGBA(255, 255, 255, 255));
-      _pColors[TEXTSELECTION]   = new Color4f(ColorRGBA( 96, 176, 255, 200));
-      _pColors[TEXTSELECTED]    = new Color4f(ColorRGBA(255, 255, 255, 255));
-      _pColors[BACKGROUND]      = new Color4f(ColorRGBA( 50,  50,  50, 255));
-      _pColors[MENUBAR    ]     = new Color4f(ColorRGBA(214, 214, 214, 255));
-      _pColors[PROGRESSBAR]     = new Color4f(ColorRGBA(  0,   0, 255, 200));
-      _pColors[SEPARATOR]       = new Color4f(ColorRGBA(255, 255, 255, 255));
-      _pColors[TEXTFIELD]       = new Color4f(ColorRGBA( 90,  90,  90, 255));
-      _pColors[CONTEXTMENU]     = new Color4f(ColorRGBA(255, 255, 255, 255));
-      _pColors[CONTEXTMENUFONT] = new Color4f(ColorRGBA(  0,   0,   0, 255));
-      _pColors[PANEL]           = new Color4f(ColorRGBA( 49, 100, 155, 100));
-      _pColors[SCROLLPANEL]     = new Color4f(ColorRGBA(200, 200, 200, 255));
-      _pColors[MAINCOMPONENTS]  = new Color4f(ColorRGBA( 51,  97, 136, 255));
-      _pColors[OVERCOMPONENTS]  = new Color4f(ColorRGBA(  0, 220, 255, 70));
-      _pColors[HOLDCOMPONENTS]  = new Color4f(ColorRGBA(116,   0, 163, 255));
-      _pColors[FONT]            = new Color4f(ColorRGBA(255, 255, 255, 255));
+      _pColors[CURSOR]          = Color4f(ColorRGBA(255, 255,   0, 255));
+      _pColors[TEXT]            = Color4f(ColorRGBA(255, 255, 255, 255));
+      _pColors[TEXTSELECTION]   = Color4f(ColorRGBA( 96, 176, 255, 200));
+      _pColors[TEXTSELECTED]    = Color4f(ColorRGBA(255, 255, 255, 255));
+      _pColors[BACKGROUND]      = Color4f(ColorRGBA( 50,  50,  50, 255));
+      _pColors[MENUBAR    ]     = Color4f(ColorRGBA(214, 214, 214, 255));
+      _pColors[PROGRESSBAR]     = Color4f(ColorRGBA(  0,   0, 255, 200));
+      _pColors[SEPARATOR]       = Color4f(ColorRGBA(255, 255, 255, 255));
+      _pColors[TEXTFIELD]       = Color4f(ColorRGBA( 90,  90,  90, 255));
+      _pColors[CONTEXTMENU]     = Color4f(ColorRGBA(255, 255, 255, 255));
+      _pColors[CONTEXTMENUFONT] = Color4f(ColorRGBA(  0,   0,   0, 255));
+      _pColors[PANEL]           = Color4f(ColorRGBA( 49, 100, 155, 100));
+      _pColors[SCROLLPANEL]     = Color4f(ColorRGBA(200, 200, 200, 255));
+      _pColors[MAINCOMPONENTS]  = Color4f(ColorRGBA( 51,  97, 136, 255));
+      _pColors[OVERCOMPONENTS]  = Color4f(ColorRGBA(  0, 220, 255, 70));
+      _pColors[HOLDCOMPONENTS]  = Color4f(ColorRGBA(116,   0, 163, 255));
+      _pColors[FONT]            = Color4f(ColorRGBA(255, 255, 255, 255));
       break;
    case WINDOWS:
       break;
    case OSX:
-	   _pColors[CURSOR]          = new Color4f(ColorRGBA(  0,   0,   0, 255));
-      _pColors[TEXT]            = new Color4f(ColorRGBA(  0,   0,   0, 255));
-	   _pColors[TEXTSELECTION]   = new Color4f(ColorRGBA( 50, 104, 166, 200));
-	   _pColors[TEXTSELECTED]    = new Color4f(ColorRGBA(255, 255, 255, 255));
-	   _pColors[BACKGROUND]      = new Color4f(ColorRGBA( 33,  59, 116, 255));
-	   _pColors[MENUBAR    ]     = new Color4f(ColorRGBA(240, 240, 240, 255));
-	   _pColors[PROGRESSBAR]     = new Color4f(ColorRGBA(106, 168, 229, 200));
-	   _pColors[SEPARATOR]       = new Color4f(ColorRGBA(115, 115, 115, 255));
-	   _pColors[TEXTFIELD]       = new Color4f(ColorRGBA(255, 255, 255, 255));
-	   _pColors[CONTEXTMENU]     = new Color4f(ColorRGBA(240, 240, 240, 255));
-	   _pColors[CONTEXTMENUFONT] = new Color4f(ColorRGBA(  0,   0,   0, 255));
-	   _pColors[PANEL]           = new Color4f(ColorRGBA( 49, 100, 155, 100));
-	   _pColors[SCROLLPANEL]     = new Color4f(ColorRGBA( 88, 139, 204, 255));
-	   _pColors[MAINCOMPONENTS]  = new Color4f(ColorRGBA(210, 210, 210, 255));
-	   _pColors[OVERCOMPONENTS]  = new Color4f(ColorRGBA(  0, 220, 255, 70));
-	   _pColors[HOLDCOMPONENTS]  = new Color4f(ColorRGBA(116,   0, 163, 255));
-	   _pColors[FONT]            = new Color4f(ColorRGBA(  0,   0,   0, 255));
+	   _pColors[CURSOR]          = Color4f(ColorRGBA(  0,   0,   0, 255));
+      _pColors[TEXT]            = Color4f(ColorRGBA(  0,   0,   0, 255));
+	   _pColors[TEXTSELECTION]   = Color4f(ColorRGBA( 50, 104, 166, 200));
+	   _pColors[TEXTSELECTED]    = Color4f(ColorRGBA(255, 255, 255, 255));
+	   _pColors[BACKGROUND]      = Color4f(ColorRGBA( 33,  59, 116, 255));
+	   _pColors[MENUBAR    ]     = Color4f(ColorRGBA(240, 240, 240, 255));
+	   _pColors[PROGRESSBAR]     = Color4f(ColorRGBA(106, 168, 229, 200));
+	   _pColors[SEPARATOR]       = Color4f(ColorRGBA(115, 115, 115, 255));
+	   _pColors[TEXTFIELD]       = Color4f(ColorRGBA(255, 255, 255, 255));
+	   _pColors[CONTEXTMENU]     = Color4f(ColorRGBA(240, 240, 240, 255));
+	   _pColors[CONTEXTMENUFONT] = Color4f(ColorRGBA(  0,   0,   0, 255));
+	   _pColors[PANEL]           = Color4f(ColorRGBA( 49, 100, 155, 100));
+	   _pColors[SCROLLPANEL]     = Color4f(ColorRGBA( 88, 139, 204, 255));
+	   _pColors[MAINCOMPONENTS]  = Color4f(ColorRGBA(210, 210, 210, 255));
+	   _pColors[OVERCOMPONENTS]  = Color4f(ColorRGBA(  0, 220, 255, 70));
+	   _pColors[HOLDCOMPONENTS]  = Color4f(ColorRGBA(116,   0, 163, 255));
+	   _pColors[FONT]            = Color4f(ColorRGBA(  0,   0,   0, 255));
       break;
    case WARCRAFT:
-      _pColors[CURSOR]          = new Color4f(ColorRGBA(255, 255, 255, 255));
-      _pColors[TEXT]            = new Color4f(ColorRGBA(255, 255, 255, 255));
-      _pColors[TEXTSELECTION]   = new Color4f(ColorRGBA( 70, 164,  76, 200));
-      _pColors[TEXTSELECTED]    = new Color4f(ColorRGBA(  0,   0,   0, 255));
-      _pColors[BACKGROUND]      = new Color4f(ColorRGBA( 50,  50,  50, 255));
-      _pColors[MENUBAR    ]     = new Color4f(ColorRGBA(223,  47,   8, 255));
-      _pColors[PROGRESSBAR]     = new Color4f(ColorRGBA(  0,  55,   0, 200));
-      _pColors[SEPARATOR]       = new Color4f(ColorRGBA(  5,  70,   0, 255));
-      _pColors[TEXTFIELD]       = new Color4f(ColorRGBA( 63, 101,  61, 255));
-      _pColors[CONTEXTMENU]     = new Color4f(ColorRGBA( 63,  94,  53, 255));
-      _pColors[CONTEXTMENUFONT] = new Color4f(ColorRGBA(219, 157,   0, 255));
-      _pColors[PANEL]           = new Color4f(ColorRGBA( 63,  94,  53, 100));
-      _pColors[SCROLLPANEL]     = new Color4f(ColorRGBA(200, 200, 200, 255));
-      _pColors[MAINCOMPONENTS]  = new Color4f(ColorRGBA(  5, 130,  15, 255));
-      _pColors[OVERCOMPONENTS]  = new Color4f(ColorRGBA(240, 240, 255, 150));
-      _pColors[HOLDCOMPONENTS]  = new Color4f(ColorRGBA(116,   0, 163, 255));
-      _pColors[FONT]            = new Color4f(ColorRGBA(219, 157,   0, 255));
+      _pColors[CURSOR]          = Color4f(ColorRGBA(255, 255, 255, 255));
+      _pColors[TEXT]            = Color4f(ColorRGBA(255, 255, 255, 255));
+      _pColors[TEXTSELECTION]   = Color4f(ColorRGBA( 70, 164,  76, 200));
+      _pColors[TEXTSELECTED]    = Color4f(ColorRGBA(  0,   0,   0, 255));
+      _pColors[BACKGROUND]      = Color4f(ColorRGBA( 50,  50,  50, 255));
+      _pColors[MENUBAR    ]     = Color4f(ColorRGBA(223,  47,   8, 255));
+      _pColors[PROGRESSBAR]     = Color4f(ColorRGBA(  0,  55,   0, 200));
+      _pColors[SEPARATOR]       = Color4f(ColorRGBA(  5,  70,   0, 255));
+      _pColors[TEXTFIELD]       = Color4f(ColorRGBA( 63, 101,  61, 255));
+      _pColors[CONTEXTMENU]     = Color4f(ColorRGBA( 63,  94,  53, 255));
+      _pColors[CONTEXTMENUFONT] = Color4f(ColorRGBA(219, 157,   0, 255));
+      _pColors[PANEL]           = Color4f(ColorRGBA( 63,  94,  53, 100));
+      _pColors[SCROLLPANEL]     = Color4f(ColorRGBA(200, 200, 200, 255));
+      _pColors[MAINCOMPONENTS]  = Color4f(ColorRGBA(  5, 130,  15, 255));
+      _pColors[OVERCOMPONENTS]  = Color4f(ColorRGBA(240, 240, 255, 150));
+      _pColors[HOLDCOMPONENTS]  = Color4f(ColorRGBA(116,   0, 163, 255));
+      _pColors[FONT]            = Color4f(ColorRGBA(219, 157,   0, 255));
       break;
    case CLEAN:
-      _pColors[CURSOR]          = new Color4f(ColorRGBA(  0,   0,   0, 255));
-      _pColors[TEXT]            = new Color4f(ColorRGBA(255, 255, 255, 255));
-      _pColors[TEXTSELECTION]   = new Color4f(ColorRGBA( 96, 176, 255, 200));
-      _pColors[TEXTSELECTED]    = new Color4f(ColorRGBA(255, 255, 255, 255));
-      _pColors[BACKGROUND]      = new Color4f(ColorRGBA(255, 255, 255, 255));
-      _pColors[MENUBAR    ]     = new Color4f(ColorRGBA(189, 199, 216, 255));
-      _pColors[PROGRESSBAR]     = new Color4f(ColorRGBA(100, 100, 255, 200));
-      _pColors[SEPARATOR]       = new Color4f(ColorRGBA(255, 255, 255, 255));
-      _pColors[TEXTFIELD]       = new Color4f(ColorRGBA(200, 200, 200, 255));
-      _pColors[CONTEXTMENU]     = new Color4f(ColorRGBA(255, 255, 255, 255));
-      _pColors[CONTEXTMENUFONT] = new Color4f(ColorRGBA(  0,   0,   0, 255));
-      _pColors[PANEL]           = new Color4f(ColorRGBA( 80,  80,  80, 200));
-      _pColors[SCROLLPANEL]     = new Color4f(ColorRGBA(200, 200, 200, 255));
-      _pColors[MAINCOMPONENTS]  = new Color4f(ColorRGBA(151, 197, 236, 255));
-      _pColors[OVERCOMPONENTS]  = new Color4f(ColorRGBA(  0, 220, 255, 70));
-      _pColors[HOLDCOMPONENTS]  = new Color4f(ColorRGBA(116,   0, 163, 255));
-      _pColors[FONT]            = new Color4f(ColorRGBA(  0,   0,   0, 255));
+      _pColors[CURSOR]          = Color4f(ColorRGBA(  0,   0,   0, 255));
+      _pColors[TEXT]            = Color4f(ColorRGBA(255, 255, 255, 255));
+      _pColors[TEXTSELECTION]   = Color4f(ColorRGBA( 96, 176, 255, 200));
+      _pColors[TEXTSELECTED]    = Color4f(ColorRGBA(255, 255, 255, 255));
+      _pColors[BACKGROUND]      = Color4f(ColorRGBA(255, 255, 255, 255));
+      _pColors[MENUBAR    ]     = Color4f(ColorRGBA(189, 199, 216, 255));
+      _pColors[PROGRESSBAR]     = Color4f(ColorRGBA(100, 100, 255, 200));
+      _pColors[SEPARATOR]       = Color4f(ColorRGBA(255, 255, 255, 255));
+      _pColors[TEXTFIELD]       = Color4f(ColorRGBA(200, 200, 200, 255));
+      _pColors[CONTEXTMENU]     = Color4f(ColorRGBA(255, 255, 255, 255));
+      _pColors[CONTEXTMENUFONT] = Color4f(ColorRGBA(  0,   0,   0, 255));
+      _pColors[PANEL]           = Color4f(ColorRGBA( 80,  80,  80, 200));
+      _pColors[SCROLLPANEL]     = Color4f(ColorRGBA(200, 200, 200, 255));
+      _pColors[MAINCOMPONENTS]  = Color4f(ColorRGBA(151, 197, 236, 255));
+      _pColors[OVERCOMPONENTS]  = Color4f(ColorRGBA(  0, 220, 255, 70));
+      _pColors[HOLDCOMPONENTS]  = Color4f(ColorRGBA(116,   0, 163, 255));
+      _pColors[FONT]            = Color4f(ColorRGBA(  0,   0,   0, 255));
       break;
    default:
       loadScheme(SCVDEFAULT);
@@ -101,36 +96,46 @@ void ColorScheme::loadScheme(ColorScheme::SchemeStyle style) {
    }
 }
 
+/*! Sets a color value in the color scheme.
+\param target Color scheme entry that should be modified.
+\param color New color of component.
+*/
 void ColorScheme::setColor(ColorScheme::Target target, const Color4f &color) {
-   (*_pColors[target]) = color;
+   _pColors[target] = color;
 }
 
+//! Returns component's current color in the color scheme.
 const Color4f& ColorScheme::getColor(ColorScheme::Target target) const {
-   return (*_pColors[target]);
+   return _pColors[target];
 }
 
+//! Prepares the graphics system to draw a component with the specified color. Should be called only by widget implementations.
 void ColorScheme::applyColor(const Color4f &color) {
    glColor4fv(&color[0]);
 }
 
+
+//! Prepares to draw component with specified color target.
 void ColorScheme::applyColor(ColorScheme::Target target) {
    if (target == ColorScheme::BACKGROUND) {
-      glClearColor((*_pColors[target])[0], (*_pColors[target])[1], (*_pColors[target])[2], (*_pColors[target])[3]);
+      glClearColor(_pColors[target][0], _pColors[target][1], _pColors[target][2], _pColors[target][3]);
    } else {
-      glColor4fv(&(*_pColors[target])[0]);
+      glColor4fv(&_pColors[target][0]);
    }
 }
 
+//! Prepares to draw a component with the specified color target, brightened/darkened by the value w.
 void ColorScheme::applyColor(ColorScheme::Target target, float w) {
+   Color4f adjusted = _pColors[target] + w;
    if (target == ColorScheme::BACKGROUND) {
-      glClearColor((*_pColors[target] + w)[0], (*_pColors[target] + w)[1], (*_pColors[target] + w)[2], (*_pColors[target])[3]);
+      glClearColor(adjusted[0], adjusted[1], adjusted[2], adjusted[3]);
    } else {
-      glColor3fv(&(*_pColors[target] + w)[0]);
+      glColor3fv(&adjusted[0]);
    }
 }
 
-void ColorScheme::applyDefaultModulate(void) {
-   glColor4fv(&_defaultModulateColor[0]);
+void ColorScheme::applyDefaultModulate() {
+   applyColor(_defaultModulateColor);
 }
 
 } // namespace scv
