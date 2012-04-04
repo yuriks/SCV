b52e3b6d89e00cb104786689d820dc831fc078eb
yuriks <yuriks@yuriks.net>
2012-04-04 14:27:57 +0000

    Fixed filename case in includes.

--- SCV/src/data/ScrollPaneData.cpp
+++ SCV/src/data/ScrollPaneData.cpp
@@ -1,4 +1,4 @@
-#include "Data.h"
+#include "data.h"
 
 namespace scv {
 namespace data {
--- scv_examples/src/example1.cpp
+++ scv_examples/src/example1.cpp
@@ -4,11 +4,11 @@ SCV - Simple Components for Visual
 Developed at Federal University of Santa Maria - UFSM
 LaCA - Computer Applied Laboratory - Laboratório de Computação Aplicada
 www.inf.ufsm.br/~pozzer/scv/
-2007-2012 
+2007-2012
 
 */
 
-#include "scv/SCV.h"
+#include "SCV/SCV.h"
 
 void example1_main() {
    scv::Kernel *kernel = scv::Kernel::getInstance();
